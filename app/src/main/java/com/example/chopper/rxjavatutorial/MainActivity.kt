99e6d45ca10edd397db68eb3052cd5dc6dce09e9
Aldi Satrio Aji <aldis85@yahoo.com>
2018-07-29 11:21:00 +0700

    first commit

---
+++ app/src/main/java/com/example/chopper/rxjavatutorial/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.chopper.rxjavatutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+
+    private var disposable: Disposable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val animalsObservable: Observable<String> = Observable.fromArray(
+                "Ant", "Ape",
+                "Bat", "Bee", "Bear", "Butterfly",
+                "Cat", "Crab", "Cod",
+                "Dog", "Dove",
+                "Fox", "Frog"
+        )
+
+        animalsObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter {
+                    it.toLowerCase().startsWith("b")
+                }
+                .subscribe(object : Observer<String> {
+                    override fun onComplete() {
+                        Toast.makeText(this@MainActivity, "Completed", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                        disposable = d
+                        Toast.makeText(this@MainActivity, "Subscribed", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onNext(t: String) {
+                        Toast.makeText(this@MainActivity, t + " ", Toast.LENGTH_SHORT).show()
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_SHORT).show()
+                    }
+                })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+
+}
